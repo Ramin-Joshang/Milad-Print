539121836f1f4cffeb8f5cf1446ac1a9b2183dc2
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-09-29 12:40:53 +0330

    category, procuts, background, blog and FAQ created

--- js/script.js
+++ js/script.js
@@ -6,8 +6,21 @@ const swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".next",
+        prevEl: ".prev",
     },
+});
 
+const swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    loop: true,
+    slidesPerView: 6,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+        nextEl: ".next-1",
+        prevEl: ".prev-1",
+    },
 });
