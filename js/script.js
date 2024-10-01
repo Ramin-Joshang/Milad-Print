9a60658a8abe856943dd43142d3e83f4cc2d0860
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-10-01 13:39:51 +0330

    header and navbar responsived

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
+const navbarDropdownCategories = document.getElementById("navar-menu-category").children;
+const navbarDropdownProducts = document.getElementById("navbar-menu-products").children;
+[...navbarDropdownCategories].forEach((element, index) => {
+    element.addEventListener("mouseover", () => {
+        [...navbarDropdownCategories].forEach(element => {
+            element.classList.remove("text-primary");
+            element.classList.remove("text-xs")
+            element.classList.add("text-[11px]")
+            element.classList.add("text-[#7B7D7E]")
+            element.classList.remove("font-bold");
+        });
+        element.classList.add("text-primary");
+        element.classList.remove("text-[11px]")
+        element.classList.add("text-xs")
+        element.classList.add("font-bold");
+        [...navbarDropdownProducts].forEach(element => {
+            element.classList.add("hidden");
+            navbarDropdownProducts[index].classList.remove("hidden");
+        })
+    })
+});
+
+
+const navbar = document.getElementById("navbar");
+const navbarBtn = document.getElementById("navbar-btn");
+const navbarBtnIcon = navbarBtn.children;
+console.log(navbarBtnIcon[0], navbarBtnIcon[1])
+console.log(navbarBtn)
+console.log(navbar)
+let isNavbarOpen = false;
+navbarBtn.addEventListener("click", () => {
+    if (isNavbarOpen) {
+        navbar.classList.add("-right-64");
+        navbar.classList.remove("-right-0");
+        navbarBtnIcon[1].classList.add("hidden");
+        navbarBtnIcon[0].classList.remove("hidden");
+        isNavbarOpen = false;
+    } else {
+        navbar.classList.remove("-right-64");
+        navbar.classList.add("-right-0");
+        navbarBtnIcon[1].classList.remove("hidden");
+        navbarBtnIcon[0].classList.add("hidden");
+        isNavbarOpen = true;
+    }
+})
+
 const swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     loop: true,
